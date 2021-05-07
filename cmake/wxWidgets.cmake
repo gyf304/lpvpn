4a4a32c9bf7accda10bdda50f903a32171eefd40
Yifan Gu <gyf304@users.noreply.github.com>
2021-05-06 23:50:28 -0400

    statically link vcruntime

--- cmake/wxWidgets.cmake
+++ cmake/wxWidgets.cmake
@@ -9,6 +9,8 @@ if(NOT wxwidgets_POPULATED)
 	FetchContent_Populate(wxwidgets)
 endif()
 
+set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
 set(wxBUILD_MONOLITHIC ON CACHE INTERNAL "")
 set(wxBUILD_SHARED OFF CACHE INTERNAL "")
+set(wxBUILD_USE_STATIC_RUNTIME ON CACHE INTERNAL "")
 add_subdirectory("${wxwidgets_SOURCE_DIR}")
